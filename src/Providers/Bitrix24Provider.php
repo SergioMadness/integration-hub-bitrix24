a3453bd7028de64ae0ca2aaf3e79538be71f71e4
Сергей Зинченко <you@example.com>
2022-07-03 17:58:24 +0300

    Register subsystem in pool

--- src/Providers/Bitrix24Provider.php
+++ src/Providers/Bitrix24Provider.php
@@ -2,13 +2,27 @@
 
 use Illuminate\Support\ServiceProvider;
 use professionalweb\IntegrationHub\Bitrix24\Services\Bitrix24Service;
+use professionalweb\IntegrationHub\Bitrix24\Models\Bitrix24DealOptions;
+use professionalweb\IntegrationHub\Bitrix24\Models\Bitrix24LeadOptions;
+use professionalweb\IntegrationHub\Bitrix24\Models\Bitrix24ContactOptions;
+use professionalweb\IntegrationHub\Bitrix24\Models\Bitrix24GetDealOptions;
+use professionalweb\IntegrationHub\Bitrix24\Models\Bitrix24GetLeadOptions;
+use professionalweb\IntegrationHub\Bitrix24\Models\Bitrix24InvoiceOptions;
 use professionalweb\IntegrationHub\Bitrix24\Services\Bitrix24DealSubsystem;
 use professionalweb\IntegrationHub\Bitrix24\Services\Bitrix24LeadSubsystem;
+use professionalweb\IntegrationHub\Bitrix24\Models\Bitrix24GetContactOptions;
+use professionalweb\IntegrationHub\Bitrix24\Models\Bitrix24GetInvoiceOptions;
+use professionalweb\IntegrationHub\Bitrix24\Models\Bitrix24GetProductOptions;
+use professionalweb\IntegrationHub\Bitrix24\Models\Bitrix24SearchDealOptions;
+use professionalweb\IntegrationHub\Bitrix24\Models\Bitrix24SearchLeadOptions;
 use professionalweb\IntegrationHub\Bitrix24\Services\Bitrix24GetDealSubsystem;
 use professionalweb\IntegrationHub\Bitrix24\Services\Bitrix24InvoiceSubsystem;
 use professionalweb\IntegrationHub\Bitrix24\Services\Bitrix24ContactSubsystem;
 use professionalweb\IntegrationHub\Bitrix24\Services\Bitrix24GetLeadSubsystem;
 use professionalweb\IntegrationHub\Bitrix24\Services\Bitrix24WorkflowSubsystem;
+use professionalweb\IntegrationHub\Bitrix24\Models\Bitrix24IsUserOnlineOptions;
+use professionalweb\IntegrationHub\Bitrix24\Models\Bitrix24SearchContactOptions;
+use professionalweb\IntegrationHub\Bitrix24\Models\Bitrix24StartWorkflowOptions;
 use professionalweb\IntegrationHub\Bitrix24\Services\Bitrix24GetProductSubsystem;
 use professionalweb\IntegrationHub\Bitrix24\Services\Bitrix24SearchDealSubsystem;
 use professionalweb\IntegrationHub\Bitrix24\Services\Bitrix24SearchLeadSubsystem;
@@ -16,7 +30,11 @@ use professionalweb\IntegrationHub\Bitrix24\Services\Bitrix24GetInvoiceSubsystem
 use professionalweb\IntegrationHub\Bitrix24\Services\Bitrix24GetContactSubsystem;
 use professionalweb\IntegrationHub\Bitrix24\Services\Bitrix24UpdateLeadSubsystem;
 use professionalweb\IntegrationHub\Bitrix24\Services\Bitrix24UpdateDealSubsystem;
+use professionalweb\IntegrationHub\Bitrix24\Models\Bitrix24CheckDuplicatesOptions;
+use professionalweb\IntegrationHub\Bitrix24\Models\Bitrix24ConvertCurrencyOptions;
 use professionalweb\IntegrationHub\Bitrix24\Services\Bitrix24IsUserOnlineSubsystem;
+use professionalweb\IntegrationHub\Bitrix24\Models\Bitrix24LeadDistributionOptions;
+use professionalweb\IntegrationHub\Bitrix24\Models\Bitrix24SetInvoiceStatusOptions;
 use professionalweb\IntegrationHub\Bitrix24\Services\Bitrix24UpdateInvoiceSubsystem;
 use professionalweb\IntegrationHub\Bitrix24\Services\Bitrix24SearchContactSubsystem;
 use professionalweb\IntegrationHub\Bitrix24\Services\Bitrix24CheckDuplicatesSubsystem;
@@ -24,6 +42,7 @@ use professionalweb\IntegrationHub\Bitrix24\Services\Bitrix24ConvertCurrencySubs
 use professionalweb\IntegrationHub\Bitrix24\Services\Bitrix24LeadDistributionSubsystem;
 use professionalweb\IntegrationHub\Bitrix24\Services\Bitrix24SetInvoiceStatusSubsystem;
 use professionalweb\IntegrationHub\Bitrix24\Interfaces\Bitrix24Service as IBitrix24Service;
+use professionalweb\IntegrationHub\IntegrationHubCommon\Interfaces\Services\SubsystemPool;
 use professionalweb\IntegrationHub\Bitrix24\Bitrix24LeadDistribution\Algorithms\RoundRobin;
 use professionalweb\IntegrationHub\Bitrix24\Bitrix24LeadDistribution\Services\DistributionService;
 use professionalweb\IntegrationHub\Bitrix24\Interfaces\Bitrix24DealSubsystem as IBitrix24DealSubsystem;
@@ -53,7 +72,33 @@ class Bitrix24Provider extends ServiceProvider
 {
     public function boot(): void
     {
+        $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'IntegrationHubBitrix24');
 
+        $this->app->booted(static function () {
+            /** @var SubsystemPool $pool */
+            $pool = app(SubsystemPool::class);
+            $pool->register(trans('IntegrationHubBitrix24::common.check-duplicates'), Bitrix24CheckDuplicatesSubsystem::BITRIX24_CHECK_DUPLICATES, new Bitrix24CheckDuplicatesOptions());
+            $pool->register(trans('IntegrationHubBitrix24::common.contact'), Bitrix24ContactSubsystem::BITRIX24_CONTACT, new Bitrix24ContactOptions());
+            $pool->register(trans('IntegrationHubBitrix24::common.convert-currency'), Bitrix24ConvertCurrencySubsystem::BITRIX24_CONVERT_CURRENCY, new Bitrix24ConvertCurrencyOptions());
+            $pool->register(trans('IntegrationHubBitrix24::common.deal'), Bitrix24DealSubsystem::BITRIX24_CREATE_DEAL, new Bitrix24DealOptions());
+            $pool->register(trans('IntegrationHubBitrix24::common.get-contact'), Bitrix24GetContactSubsystem::BITRIX24_GET_CONTACT, new Bitrix24GetContactOptions());
+            $pool->register(trans('IntegrationHubBitrix24::common.get-deal'), Bitrix24GetDealSubsystem::BITRIX24_GET_DEAL, new Bitrix24GetDealOptions());
+            $pool->register(trans('IntegrationHubBitrix24::common.get-invoice'), Bitrix24GetInvoiceSubsystem::BITRIX24_GET_INVOICE, new Bitrix24GetInvoiceOptions());
+            $pool->register(trans('IntegrationHubBitrix24::common.get-lead'), Bitrix24GetLeadSubsystem::BITRIX24_GET_LEAD, new Bitrix24GetLeadOptions());
+            $pool->register(trans('IntegrationHubBitrix24::common.get-product'), Bitrix24GetProductSubsystem::BITRIX24_GET_CONTACT, new Bitrix24GetProductOptions());
+            $pool->register(trans('IntegrationHubBitrix24::common.invoice'), Bitrix24InvoiceSubsystem::BITRIX24_INVOICE, new Bitrix24InvoiceOptions());
+            $pool->register(trans('IntegrationHubBitrix24::common.is-user-online'), Bitrix24IsUserOnlineSubsystem::BITRIX24_GET_EMPLOYEE_STATUS, new Bitrix24IsUserOnlineOptions());
+            $pool->register(trans('IntegrationHubBitrix24::common.lead-distribution'), Bitrix24LeadDistributionSubsystem::BITRIX24_LEAD_DISTRIBUTION, new Bitrix24LeadDistributionOptions());
+            $pool->register(trans('IntegrationHubBitrix24::common.lead'), Bitrix24LeadSubsystem::BITRIX24_LEAD, new Bitrix24LeadOptions());
+            $pool->register(trans('IntegrationHubBitrix24::common.search-contact'), Bitrix24SearchContactSubsystem::BITRIX24_SEARCH_CONTACT, new Bitrix24SearchContactOptions());
+            $pool->register(trans('IntegrationHubBitrix24::common.search-deal'), Bitrix24SearchDealSubsystem::BITRIX24_SEARCH_DEAL, new Bitrix24SearchDealOptions());
+            $pool->register(trans('IntegrationHubBitrix24::common.search-lead'), Bitrix24SearchLeadSubsystem::BITRIX24_SEARCH_LEAD, new Bitrix24SearchLeadOptions());
+            $pool->register(trans('IntegrationHubBitrix24::common.set-invoice-status'), Bitrix24SetInvoiceStatusSubsystem::BITRIX24_SET_INVOICE_STATUS, new Bitrix24SetInvoiceStatusOptions());
+            $pool->register(trans('IntegrationHubBitrix24::common.update-deal'), Bitrix24UpdateDealSubsystem::BITRIX24_UPDATE_DEAL, new Bitrix24DealOptions());
+            $pool->register(trans('IntegrationHubBitrix24::common.update-invoice'), Bitrix24UpdateInvoiceSubsystem::BITRIX24_UPDATE_INVOICE, new Bitrix24InvoiceOptions());
+            $pool->register(trans('IntegrationHubBitrix24::common.update-lead'), Bitrix24UpdateLeadSubsystem::BITRIX24_UPDATE_LEAD, new Bitrix24LeadOptions());
+            $pool->register(trans('IntegrationHubBitrix24::common.start-workflow'), Bitrix24WorkflowSubsystem::BITRIX24_WORKFLOW, new Bitrix24StartWorkflowOptions());
+        });
     }
 
     public function register(): void
---
+++ src/resources/lang/ru/common.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'check-duplicates'   => 'Проверка дубликатов',
+    'contact'            => 'Создание контакта',
+    'convert-currency'   => 'Конвертация валют',
+    'deal'               => 'Создание сделки',
+    'get-contact'        => 'Запрос контакта',
+    'get-deal'           => 'Запрос сделки',
+    'get-invoice'        => 'Запрос счета',
+    'get-lead'           => 'Запрос лида',
+    'get-product'        => 'Запрос продукта',
+    'invoice'            => 'Создание счета',
+    'is-user-online'     => 'Проверка статуса пользователь',
+    'lead-distribution'  => 'Распределение лидов',
+    'lead'               => 'Создание лида',
+    'search-contact'     => 'Поиск контакта',
+    'search-deal'        => 'Поиск сделки',
+    'search-lead'        => 'Поиск лида',
+    'set-invoice-status' => 'Установка статуса счета',
+    'update-deal'        => 'Обновление сделки',
+    'update-invoice'     => 'Обновление счета',
+    'update-lead'        => 'Обновление лида',
+    'start-workflow'     => 'Запуск бизнес-процесса',
+];
